83273dea903abb066fd883c84c24db0a5d75c038
Nargiza Konushalieva <64344753+Lebedok@users.noreply.github.com>
2020-12-10 12:01:09 -0600

    Add files via upload

---
+++ WhileLoopExample.java
@@ -0,0 +1,14 @@
+package WhileLoop;
+
+public class WhileLoopExample {
+    public static void main(String[] args) {
+
+        int num = 20;
+
+        while (num <= 40) {
+
+            System.out.println(num);
+            num++;
+        }
+    }
+}
